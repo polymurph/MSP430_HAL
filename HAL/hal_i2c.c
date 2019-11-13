f8176d822bc2480b3ceb77b2bbffa8809b0b617b
polymurph <eddyed.k@gmail.com>
2019-11-13 10:02:01 +0100

    added i2c .c/h

---
+++ HAL/hal_i2c.c
@@ -0,0 +1,13 @@
+/*
+ * hal_i2c.c
+ *
+ *  Created on: Nov 13, 2019
+ *      Author: Edwin
+ */
+
+#include "hal_i2c.h"
+#include "msp430fr6989.h"
+
+#include <stdint.h>
+
+
---
+++ HAL/hal_i2c.h
@@ -0,0 +1,16 @@
+/*
+ * hal_i2c.h
+ *
+ *  Created on: Nov 13, 2019
+ *      Author: Edwin
+ */
+
+#ifndef HAL_HAL_I2C_H_
+#define HAL_HAL_I2C_H_
+
+#include <stdint.h>
+
+
+
+
+#endif /* HAL_HAL_I2C_H_ */
