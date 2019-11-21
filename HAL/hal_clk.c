4bcd2203da62df843f523e5f2d46216b4f94d720
polymurph <eddyed.k@gmail.com>
2019-11-21 17:08:06 +0100

    added hal_config_LFXT

--- HAL/hal_clk.c
+++ HAL/hal_clk.c
@@ -20,15 +20,48 @@ static inline void _lock_registers()
     CSCTL0 &= ~CSKEY;
 }
 
+clk_error_t hal_clk_config_LFXT(drive_strenght_t strength ,bool bypass, bool enable)
+{
+    _unlock_registers();
+
+    // drive strength
+    CSCTL4 &= ~LFXTDRIVE_3;
+    CSCTL4 |= (uint16_t)(strength << 6);
+
+
+    if(bypass){
+        CSCTL4 |= LFXTBYPASS;
+    } else {
+        CSCTL4 &= ~LFXTBYPASS;
+    }
+
+    if(enable){
+        CSCTL4 &= ~LFXTOFF;
+    } else {
+        CSCTL4 |= LFXTOFF;
+    }
+    _lock_registers();
+
+    if(CSCTL5 & LFXTOFF) return clk_error_LFXT_FAULT;
+
+    return clk_error_NO_ERROR
+}
+
+
+
+
+
 void hal_clk_config_ACLK(clk_ACLK_src_t source, clk_presc_t prescaler, bool enable)
 {
     _unlock_registers();
 
+    // select source
+    CSCTL2 &= ~(uint16_t)(0x7 << 8);
+    CSCTL2 |= source;
+
     // set prescaler
     CSCTL3 &= ~(uint16_t)(0x7 << 8);
-    CSCTL3 |= prescaler;
-
-
+    CSCTL3 |= (uint16_t)(prescaler << 8);
 
     _lock_registers();
 }
--- HAL/hal_clk.h
+++ HAL/hal_clk.h
@@ -12,18 +12,30 @@
 #include <stdbool.h>
 
 typedef enum{
+    clk_error_NO_ERROR = 0,
+    clk_error_LFXT_FAULT,
+
+}clk_error_t;
+
+typedef enum{
     clk_ACLK_src_LFXT,
     clk_ACLK_src_HFXT
 }clk_ACLK_src_t;
 
 typedef enum{
-    clk_presc_DIV_1,
+    clk_presc_DIV_1 = 0,
     clk_presc_DIV_2,
     clk_presc_DIV_4,
     clk_presc_DIV_8,
     clk_presc_DIV_16,
     clk_presc_DIV_32
 }clk_presc_t;
+
+typedef enum{
+    drive_strenght_LOW = 0,
+    drive_strenght_INCREASED = 1,
+    drive_strenght_MAX = 3
+}drive_strenght_t;
 
 
 #endif /* HAL_HAL_CLK_H_ */
