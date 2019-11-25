7cb6ce7592d1b84bf44d2cea949af3046a21b77e
rid3onb1tch <stephan.bl@icloud.com>
2019-11-25 13:47:23 +0100

    added .c/.h files

---
+++ HAL/hal_gpio.c
@@ -0,0 +1,8 @@
+/*
+ * hal_gpio.c
+ *
+ *  Created on: 25.11.2019
+ *      Author: stephanblattler
+ */
+
+
---
+++ HAL/hal_gpio.h
@@ -0,0 +1,13 @@
+/*
+ * hal_gpio.h
+ *
+ *  Created on: 25.11.2019
+ *      Author: stephanblattler
+ */
+
+#ifndef HAL_HAL_GPIO_H_
+#define HAL_HAL_GPIO_H_
+
+
+
+#endif /* HAL_HAL_GPIO_H_ */
