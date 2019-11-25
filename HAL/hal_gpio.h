77b1e6bad96195f6cb3ecaad8957ab8563f93b3c
rid3onb1tch <stephan.bl@icloud.com>
2019-11-25 14:33:52 +0100

    started working on basic structure

--- HAL/hal_gpio.h
+++ HAL/hal_gpio.h
@@ -2,11 +2,34 @@
  * hal_gpio.h
  *
  *  Created on: 25.11.2019
- *      Author: stephanblattler
+ *      Author: stephanblaettler
  */
 
 #ifndef HAL_HAL_GPIO_H_
 #define HAL_HAL_GPIO_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "msp..
+
+
+typedef enum{
+    pin_mode_INPUT,
+    pin_mode_INPUT_PULLDOWN,
+    pin_mode_INPUT_PULLUP,
+    pin_mode_OUTPUT,
+    // gpio_mode_OUTPUT_PULLUP // weak pull up/down instead of source
+    pin_mode_PRIMARY_FUNCTION,
+    pin_mode_SECCONDARY_FUNCTION,
+    pin_mode_TERTIARY_FUNCTION
+    
+    
+}pin_mode_t;
+
+
+void hal_gpio_pin_init(pin_mode_t mode,)
+
+void hal_gpio_pin_set(pin,bool logic_level)
 
 
 
