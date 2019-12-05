8bc22dfae6fac9fd68218380b36fb9adb891d655
BergTim <timon@bergmann.ch>
2019-12-05 17:56:48 +0100

    rx,tx revisited

--- main.c
+++ main.c
@@ -8,14 +8,27 @@
 /**
  * main.c
  */
+
+void init_chip_select()
+{
+    PM5CTL0 &= ~LOCKLPM5;
+
+    P2DIR |= 0x10;
+    P2OUT &= ~0x10;
+
+    PM5CTL0 |= LOCKLPM5;
+}
+
 //Chip-Select Port 2.4
 void chip_select(bool select)
 {
+
     if(select){
         P2OUT |= 0x10;
     } else {
         P2OUT &= ~0x10;
     }
+
 }
 
 
@@ -34,23 +47,41 @@ int main(void)
 
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+	init_chip_select();
+
+/*
 	hal_clk_config_MCLK(clk_MCLK_src_LFXT, 0, true);
 	hal_clk_config_ACLK(clk_ACLK_src_LFXT, 0, true);
 
 	hal_clk_output_ACLK_to_GPIO(true);
 	hal_clk_output_MCLK_to_GPIO(true);
+*/
+	hal_spi_init(spi_mode_MASTER,spi_clk_source_ACLK, spi_clk_mode_2, 2, true);
 
-	hal_spi_init(spi_mode_MASTER, spi_clk_source_ACLK, spi_clk_mode_2, 2, true);
 
 
-	//chip_select(true);
-	hal_spi_trx_byte(0xAA);
-	//chip_select(false);
 
 	while(1)
 	{
-	    hal_spi_tx(data, sizeof(data));
-	    for(i = 0; i < 0xFF; i++);
+	    chip_select(true);
+
+        hal_spi_tx_byte(0x0F);
+        hal_spi_tx_byte(0xFF);
+
+        chip_select(false);
+
+	    //hal_spi_tx(data, sizeof(data));
+	    for(i = 0; i < 0xFFF; i++);
+
+	    chip_select(true);
+
+        hal_spi_tx_byte(0x0C);
+        hal_spi_tx_byte(0xFFF);
+
+        chip_select(false);
+
+        for(i = 0; i < 0xFF; i++);
+
 	}
 
 	return 0;
